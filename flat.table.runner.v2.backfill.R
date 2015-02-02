e7703c9d0a47850f872d1c66d2608b7063a61163
Saptarshi Guha <saptarshi.guha@gmail.com>
2015-02-02 11:25:34 -0800

    First Checkin

---
+++ flat.table.runner.v2.backfill.R
@@ -0,0 +1,81 @@
+source("~/tmp/makeFlatTables.v3.R")
+store.path <- "/user/sguha/fhraggr"
+
+
+extract.date <- function(s){
+    a <- tail(strsplit(s,"/",fixed=TRUE)[[1]],1)
+    list(name=a, date=as.Date(a))
+}
+
+waitForJobs <- function(s,L){
+    resus <- lapply(L, function(ajob){
+        rhstatus(ajob,mon.sec=Inf)
+    })
+    resusText <- (lapply(resus, function(ajob){
+        tryCatch({
+            jobname <- ajob$jobname
+            rerrors <- isn(ajob$counters$R_UNTRAPPED_ERRORS)
+            totalerrors <- sum(rerrors[,1])
+            mapinput <- ajob$counters$"Map-Reduce Framework"["Map input records",]
+            data.table(name = jobname, pct = round(100*rerrors[,1]/mapinput,1), name = rownames(rerrors))
+        },error=function(e){ data.table(name=sprintf("could not get errors for %s",ajob$jobname), pct=0, name="MISS")})
+    }))
+    embody <- paste(c(hdfs.getwd(),"\n",capture.output(print(rbindlist(resusText)))), collapse="\n")
+    email(subj=sprintf("BACKFILL FOR %s done",s), body=embody)
+    list(TRUE,embody)
+}
+    
+
+## Recreate the data
+pathnames <- sort(rhls("/user/sguha/fhr/samples/backup")$file)
+
+BACK <- 170
+
+
+for( p in X){
+    dt <- extract.date(p)
+    fileOriginDate <- dt$date
+    MS <- 0
+    rhmkdir(sprintf("%s/%s", store.path,dt$name))
+    hdfs.setwd(sprintf("%s/%s", store.path,dt$name))
+
+    print(sprintf("Processing for %s", dt$name))
+    timeperiod <- list(start = strftime(fileOriginDate-BACK,"%Y-%m-%d"), end   = strftime(fileOriginDate-7,"%Y-%m-%d"))
+    PARAM <- list(needstobetagged=TRUE,whichdate=strftime(dt$date,"%Y%m%d"),statcomputer=computeAllStats,usedt=FALSE)
+    
+    input.path <- sqtxt(sprintf("%s/1pct/",p))
+    ## Weekly Summary
+    timeChunksWk <- weekTimeChunk(timeperiod$start, timeperiod$end)
+    zweek <- rhwatch(map=summaries, reduce=rhoptions()$temp$colsummer, input=input.path
+                     ,debug='collect'
+                     ,output='rweek'
+                     ,jobname=sprintf("Weekly [ %s ]",dt$name)
+                     ,mon.sec=MS
+                     ,setup=expression({ library(rjson) })
+                     ,read=FALSE
+                     ,param=list(PARAM=append(PARAM, list(granularity='week' ,listOfTimeChunks = timeChunksWk))))
+
+    ## Monthly Summary
+    timeChunksMonth <- monthTimeChunk(timeperiod$start, timeperiod$end)
+    zmonth <- rhwatch(map=summaries, reduce=rhoptions()$temp$colsummer, input=input.path
+                      ,debug='collect'
+                      ,output='rmonth'
+                      ,mon.sec=MS
+                      ,jobname=sprintf("Monthly [ %s ]",dt$name)
+                      ,setup=expression({ library(rjson) })
+                      ,read=FALSE
+                      ,param=list(PARAM=append(PARAM, list(granularity='month' ,listOfTimeChunks = timeChunksMonth))))
+
+    ## Daily Summary
+    timeChunksDay <- dayTimeChunk(timeperiod$start, timeperiod$end)
+    zday <- rhwatch(map=summaries, reduce=rhoptions()$temp$colsummer, input=input.path
+                    ,debug='collect'
+                    ,output='rday'
+                    ,mon.sec=MS
+                    ,jobname=sprintf("Daily [ %s ]",dt$name)
+                    ,setup=expression({ library(rjson) })
+                    ,read=FALSE
+                    ,param=list(PARAM=append(PARAM, list(granularity='day' ,listOfTimeChunks = timeChunksDay))))
+    print(waitForJobs(p,list(zweek, zmonth, zday)))
+}
+email("ALL BACKFILLS DONE")
---
+++ makeFlatTables.v3.R
@@ -0,0 +1,158 @@
+                                   
+dayTimeChunk <- function(fr, to){
+    lapply(strftime(seq(from=as.Date(fr),to=as.Date(to),by=1),"%Y-%m-%d"),function(s){ c(start=s, end=s)})
+}
+weekTimeChunk <- function(fr,to){
+    ## Sunday is the first day of a week For example, 25 Dec 2013 was a Wednesday and hence belonged to the week
+    ## starting Sunday 21st if 'fr' is not a sunday it will be rewinded to the Sunday it belongs if 'to' is not a
+    ## Saturday, it will be rewinded to the previous Saturday(end of week)
+    sundaystart <- as.Date(fr)-as.POSIXlt(as.Date(fr))$wday
+    weeks <- seq(from=sundaystart,to=as.Date(to), by="weeks") 
+    m <-lapply(weeks,function(s) c(start=strftime(s,"%Y-%m-%d"), end=strftime(s+7-1,"%Y-%m-%d")))
+    names(m) <- NULL;m
+}
+
+monthTimeChunk <- function(fr,to){
+    require(zoo)
+    ## round 'fr' down to beginning of month and
+    ## round 'to' to beginning of the month
+    fr1 <- as.Date(as.yearmon(fr, "%Y-%m-%d"), frac = 0)
+    to1 <- as.Date(as.yearmon(to, "%Y-%m-%d"), frac = 0)
+    lapply(seq(fr1,to1, by='month'),function(s){
+        c(start = strftime(s,"%Y-%m-%d"), end = strftime(as.Date(as.yearmon(s, "%Y-%m-%d"), frac = 1),"%Y-%m-%d"))
+    })
+}
+
+quarterTimeChunk <- function(years){
+    ## ReWrite
+    a <- list()
+    unlist(lapply(years,function(year){
+        list(c(start=sprintf("%s-01-01",year),end=sprintf("%s-03-31",year)),
+             c(start=sprintf("%s-04-01",year),end=sprintf("%s-06-30",year)),
+             c(start=sprintf("%s-07-01",year),end=sprintf("%s-09-30",year)),
+             c(start=sprintf("%s-10-01",year),end=sprintf("%s-12-31",year)))
+    }),rec=FALSE)
+}
+    
+
+
+getDimensions <- function(b){
+    channel = isn(b$geckoAppInfo$updateChannel, "missing")
+    os      = isn(b$geckoAppInfo$os, "missing")
+    osdetail= local({
+        if(os=="WINNT"){
+            WNVer((b$data$last$ org.mozilla.sysinfo.sysinfo$version))
+        }else os
+    })
+    locale  = isn(b$data$last$org.mozilla.appInfo.appinfo$locale, "missing")
+    geo     = isn(b$geo)
+    version = isn(b$geckoAppInfo$version,"missing")
+    list(vendor=isn(b$gecko$vendor,'missing'),name = isn(b$gecko$name,"missing"),channel=channel, os=os, osdetail=osdetail, locale=locale, geo=geo,version=version)
+}
+
+computeActives          <- function(days)    if(length(days)>0) 1 else 0
+computeExistingProfiles <- function(profileCrDate,timeChunk) if(profileCrDate < timeChunk['start']) 1 else 0
+computeNewProfiles      <- function(profileCrDate,timeChunk) if(profileCrDate >= timeChunk['start'] && profileCrDate <= timeChunk['end']) 1 else 0
+computeTotalProfiles    <- function(profileCrDate,timeChunk) if(profileCrDate <= timeChunk['end']) 1 else 0
+computeTotalSeconds     <- function(days)
+{
+    sum(unlist(Filter(function(s) s>=0,lapply(days, function(dc){ c(dc$org.mozilla.appSessions.previous$cleanTotalTime,dc$org.mozilla.appSessions.previous$abortedTotalTime)}))))
+}
+computeActiveSeconds    <- function(days)
+{
+    sum(unlist(Filter(function(s) s>=0,lapply(days, function(dc){ c(dc$org.mozilla.appSessions.previous$cleanActiveTicks,dc$org.mozilla.appSessions.previous$abortedActiveTicks)}))))*5
+}
+computeNumSessions    <- function(days)
+{
+    length(unlist(Filter(function(s) s>=0,lapply(days, function(dc){ c(dc$org.mozilla.appSessions.previous$main) }))))
+}
+
+computeTotalCrashes     <- function(days)  sum(unlist(lapply(days,function(dc) c(dc$org.mozilla.crashes.crashes[c("main-crash", "content-crash")]))))
+computeTotalSearches    <- function(days,regex,negate=FALSE) {
+    sum(unlist(lapply(days,function(dc){
+        x <- dc$org.mozilla.searches.counts;
+        x[["_v"]] <- NULL
+        whichnames <- grepl(regex,names(x))
+        whichnames <- if(negate) !whichnames else whichnames
+        unlist(x [ whichnames ])
+    })))
+}
+computeIsDefault        <- function(days) 1*(sum(unlist(lapply(days,function(s) s$org.mozilla.appInfo.appinfo$isDefaultBrowser))) > 0.5*length(days))
+compute5outOf7          <- function(days,alldays,granularity,timeChunk){
+    if(granularity=="day"){
+        beg <- strftime(as.Date(timeChunk['start'])-6,"%Y-%m-%d")
+        last7days <- alldays [ names(alldays)>=beg  & names(alldays)<=timeChunk['start']]
+        1*(length(last7days)>=5)
+    }else if (granularity=="week"){
+        last7days <- days
+        1*(length(last7days)>=5)
+    }
+    else NA
+}
+
+
+computeAllStats <- function(days,control){
+    c(
+        tActives          = isn(computeActives(days),0),
+        tTotalProfiles    = isn(computeTotalProfiles(control$profileCrDate,control$timeChunk),0),
+        tExistingProfiles = isn(computeExistingProfiles(control$profileCrDate,control$timeChunk),0),
+        tNewProfiles      = isn(computeNewProfiles(control$profileCrDate,control$timeChunk),0),
+        tTotalHours       = isn(computeTotalSeconds(days),0),
+        tActiveHours      = isn(computeActiveSeconds(days),0),
+        tNumSessions      = isn(computeNumSessions(days),0),
+        tCrashes          = isn(computeTotalCrashes(days),0),
+        tGoogleSearch     = isn(computeTotalSearches(days,regex="google"),0),
+        tYahooSearch      = isn(computeTotalSearches(days,regex="yahoo"),0),
+        tBingSearch       = isn(computeTotalSearches(days,regex="bing"),0),
+        tOthersSearch     = isn(computeTotalSearches(days,regex="(google|yahoo|bing)",negate=TRUE),0),
+        tIsDefault        = isn(computeIsDefault(days),0),
+        t5outOf7          = isn(compute5outOf7(days, alldays = control$alldays,granularity =control$granularity,timeChunk = control$timeChunk),0)
+        )
+}
+    
+    
+summaries <- function(a,b){
+        if(PARAM$needstobetagged){
+            b <- fromJSON(b)
+            b$data$days <- tagDaysByBuildVersion(b)
+        }
+        bdim              <- getDimensions(b)
+        bdim$snapshot     <- PARAM$whichDate
+        bdim$granularity  <- PARAM$granularity
+        profileCrDate     <- strftime(  as.Date(b$data$last$org.mozilla.profile.age$profileCreation,"1970-01-01"), "%Y-%m-%d")
+        if(is.null(profileCrDate)) {
+            profileCrDate <- if(length(b$data$days) > 0) min(names(b$data$days)) else b$thisPingDate
+        }
+        if(is.null(profileCrDate) || is.na(profileCrDate)) return()
+        lapply(PARAM$listOfTimeChunks,function(timeChunk){
+            days           <- b$data$days [ names(b$data$days)>=timeChunk['start']  & names(b$data$days)<= timeChunk['end']]
+            bdim$timeStart <- timeChunk['start']
+            bdim$timeEnd   <- timeChunk['end']
+            ## Your custome code can be here (in statcomputer)
+            mystats        <- PARAM$statcomputer(days, control=list(alldays = b$data$days,profileCrDate=profileCrDate
+                                                           , granularity = PARAM$granularity, timeChunk=timeChunk))
+            if(PARAM$usedt){
+                rhcollect(sample(1:1000,1), cbind( as.data.table( bdim), as.data.table(as.list(mystats)))) 
+            }else{
+                rhcollect(bdim,mystats)
+            }
+        })
+    }
+
+################################################################################
+## Examples
+################################################################################
+
+## PARAM <- list(needstobetagged=FALSE,whichdate=strftime(Sys.Date(),"%Y%m%d"), granularity='week'
+##               ,listOfTimeChunks = weekTimeChunk(timeperiod$start, timeperiod$end),statcomputer=computeAllStats,usedt=FALSE)
+
+## z <- rhwatch(map=summaries, reduce=rhoptions()$temp$colsummer, input="/user/sguha/fhr/samples/output/fromjson1pct"
+##              ,debug='collect'
+##              ,output='testnew'
+##              ,setup=expression({suppressPackageStartupMessages(library(data.table))})
+##              ,param=list(PARAM=PARAM))
+
+## z2 <- make.dt(z,c(names(z[[1]][[1]]),names(z[[1]][[2]])))
+
+
+
