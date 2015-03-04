ac5d89a1ceff84ba38206d32be0c80a06d120a6d
Dave Zeber <dzeber@mozilla.com>
2015-03-04 17:52:12 -0600

    Added functions for computing search counts over a time period

--- lib/search.R
+++ lib/search.R
@@ -81,15 +81,24 @@ allSearches <- function(days) {
 ## results will not include it. To do this, set the appropriate grouping
 ## argument to NULL. Seting both to NULL means computing overall total searches.
 ##
+## If either of the grouping functions can return NA, the counts for groups
+## labelled NA can be removed from the output by setting removeNA = TRUE.
+## This can be used to restrict counts to a subset of interest: make the 
+## grouping functions return NA for other searches and drop them from the
+## output. If there is grouping along both dimensions, this will remove counts
+## for which the group labels for either provider or sap is NA. By default,
+## NA groups are not removed and are treated the same as the other groups.
+##
 ## The retun value depends on which of the provider and SAP dimensions are used
 ## for counting. If both grouping arguments are NULL, the result is a scalar 
 ## count of overall total searches for that day. If one grouping argument is 
 ## NULL, the result is a named vector mapping group names to search counts for 
 ## that group. If both grouping dimensions are used, the result is a 
 ## two-dimensional array, with provider groups as row names and SAP groups as 
-## column names.
+## column names. If removeNA caused all search counts to be removed, the result
+## is NULL.
 searchCountValues <- function(searchday, provider.grouping = identity, 
-                                                sap.grouping = identity) {
+                                    sap.grouping = identity, removeNA = FALSE) {
     ## If both grouping arguments are NULL, just return the total count.
     if(is.null(provider.grouping) && is.null(sap.grouping))
         return(sum(searchday$count))
@@ -101,6 +110,123 @@ searchCountValues <- function(searchday, provider.grouping = identity,
         groupby[[length(groupby) + 1]] <- sap.grouping(searchday$sap)
     ## Convert grouping variables to factor explicity to retain NAs.
     groupby <- lapply(groupby, factor, exclude = NULL)
-    tapply(searchday$count, groupby, sum, simplify = TRUE)
+    searchcounts <- tapply(searchday$count, groupby, sum, simplify = TRUE)
+    if(removeNA) {
+        toremove <- lapply(dimnames(searchcounts), is.na)
+        searchcounts <- do.call("[", c(searchcounts, toremove, drop = FALSE))
+        if(length(searchcounts) == 0) return(NULL)
+    }
+    searchcounts
 }
 
+## Computes daily search counts by search provider and SAP.
+##
+## Input is either the data$days list (default), or the list returned 
+## by allSearches(). The input type should be flagged by setting 'preprocess' 
+## to TRUE for the former and FALSE for the latter. 
+##
+## Search counts can be returned either as overall daily totals, or grouped
+## or subsetted by either search provider name or SAP name. This behaviour
+## is controlled using the 'provider' and 'sap' arguments. 
+##
+## In grouping, the search counts will be assigned to groups based on the 
+## search providers or SAP string identifiers. The method for assigning groups
+## can be specified in a variety of ways, which apply to both 'provider' and 
+## 'sap' arguments: 
+##  - TRUE, meaning that there will be a separate count for each raw identifier
+##    (no grouping). This is the default.
+##  - FALSE or NULL, meaning that counts will not be split across this dimension
+##    (ie. all values belong to a single, trivial group)
+##  - A function which maps a vector of identifiers to group names (or NA)
+##  - A list whose names are the group names, and whose elements are functions
+##    mapping a vector of identifiers to a vector of booleans indicating whether
+##    each should be assigned to that group. Identifiers that are not assigned
+##    to any group by any of these functions will be assigned to a group named NA.
+##    To specify another for this "unassigned" group, the list should contain
+##    an entry NA whose name is the desired group name. An example of this form
+##    is: list(yahoo = function() {...}, google = function() {...}, other = NA).
+##  An input in any different form generates an error.
+## 
+## By default, any searches assigned to a group labelled NA will be treated
+## the same as any other group. However, setting removeNA = TRUE will cause
+## the NA group to be removed from the output. This can be used for subsetting
+## searches. To only count searches belonging to a certain subset of interest, 
+## assign the others to a group labelled NA (either explicitly in the function
+## form or automatically using the list form), and use removeNA = TRUE. If both
+## the provider and sap dimensions have NAs, this will drop counts for which 
+## either dimension is NA.
+## 
+## Returns a list with an entry for each day that has searches (as returned by
+## allSearches()), except for days for which all searches were removed by 
+## setting removeNA = TRUE. If there are no such days, returns NULL.
+## Each entry is of the form returned by searchCountValues(), either a scalar
+## or a one- or two-dimensional array.
+dailySearchCounts <- function(days, provider = TRUE, sap = TRUE, 
+                                        removeNA = FALSE, preprocess = TRUE) {
+    if(preprocess) days <- allSearch(days)
+    if(length(days) == 0) return(NULL)
+    searchcounts <- lapply(days, searchCountValues, groupingFunction(provider),
+        groupingFunction(sap), removeNA)
+    ## Remove any NULLs that were introduced by subsetting.
+    if(removeNA) {
+        sc.null <- unlist(lapply(searchcounts, is.null))
+        if(all(sc.null)) return(NULL)    
+        if(any(sc.null)) return(searchcounts[!sc.null])
+    }
+    searchcounts
+}
+
+## Computes total search counts by search provider and SAP across all days
+## in the time period that have searches. 
+##
+## Arguments are as described for dailySearchCounts().
+##
+## The return value is the same as that for searchCountValues().
+totalSearchCounts <- function(days, provider = TRUE, sap = TRUE, 
+                                        removeNA = FALSE, preprocess = TRUE) {
+    if(preprocess) days <- allSearch(days)
+    if(length(days) == 0) return(NULL)
+    searches <- list(provider = unlist(lapply(days, "[[", "provider")),
+        sap = unlist(lapply(days, "[[", "sap")),
+        count = unlist(lapply(days, "[[", "count")))
+    searchCountValues(searches, groupingFunction(provider), 
+        groupingFunction(sap), removeNA)
+}
+
+
+## Generates a function to apply for grouping in searchCountValues()
+## based on the various input types accepted by the SearchCounts functions.
+groupingFunction <- function(grouping) {
+    if(isTRUE(grouping)) return(identity)
+    if(is.null(grouping) || identical(grouping, FALSE)) return(NULL)
+    if(is.function(grouping)) return(grouping)
+    if(is.list(grouping)) {
+        ## First check if we have a label for the NA group. 
+        nagroup <- is.na(grouping)
+        nagroupname <- if(any(nagroup)) {
+            grouping <- grouping[!nagroup]
+            names(grouping)[nagroup][[1]] 
+        } else { NA }
+        fun <- eval(bquote(function(vals) {
+            ## Assign values to each group as appropriate.
+            ## Keep track of unassigned values.
+            unassigned <- rep_len(TRUE, length(vals))
+            for(groupname in groupmembers) {
+                incurrentgroup <- groupmembers[[groupname]](vals)
+                if(!any(incurrentgroup)) next
+                vals[incurrentgroup] <- groupname
+                unassigned <- unassigned & !incurrentgroup
+                if(!any(unassigned)) break
+            }
+            if(any(unassigned)) vals[unassigned] <- .(unassignedgroupname)
+        }, list(unassignedgroupname = nagroupname)))
+        fun.env <- new.env(parent = emptyenv())
+        assign("groupmembers", grouping, envir = fun.env)
+        environment(fun) <- fun.env
+        return(fun)
+    }
+    ## At this point, the input doesn't match any of the valid forms.
+    stop("Invalid grouping argument".)
+}
+
+                                                    
